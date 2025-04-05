6ffeb57f09a8998041ae0f8e2121af35c0994eb3
nicoo <nicoo@mur.at>
2025-04-05 15:47:06 +0000

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    devShells."x86_64-linux".default = let
+      pkgs = import nixpkgs { system = "x86_64-linux"; config.allowAliases = false; };
+      inherit (pkgs) lib;
+    in pkgs.mkShellNoCC {
+      buildInputs = with pkgs; [
+        kind
+        kubectl
+        (pulumi.withPackages (pu: [ pu.pulumi-python ]))
+        (python3.withPackages (py: [ py.pip ]))
+      ];
+
+      PULUMI_CONFIG_PASSPHRASE = "pyGraz-k8s-test";
+    };
+  };
+}
