d54b69b68c71c3ff6326153fb7f8af1cbfeb81d8
nicoo <nicoo@mur.at>
2025-04-11 07:23:55 +0000

    cilium: use k8s.helm.v4 API [WiP]

--- cilium/__init__.py
+++ cilium/__init__.py
@@ -1,38 +1,39 @@
 from collections.abc import Set
+from functools import reduce
 from typing import Literal
 
 import pulumi
-from pulumi_kubernetes.helm.v3 import Release, ReleaseArgs, RepositoryOptsArgs
+import pulumi_kubernetes as k8s
 
-def deploy(cfg: pulumi.Config, *, features: Set[Literal['hubble']] = frozenset()):
+def deploy(cfg: pulumi.Config, *, features: Set[Literal["hubble"]] = frozenset()):
     # address and port of the k8s API server to use
     host, port = cfg.require("k8sEndpoint").rsplit(":", 1)
 
-    cilium = Release(
+    chart = k8s.helm.v4.Chart(
         "cilium",
-        ReleaseArgs(
-            chart = "cilium",
-            repository_opts = RepositoryOptsArgs(
-                repo = "https://helm.cilium.io/",
-            ),
-            version = "1.17.2",  # TODO: autoupdate?
-            namespace = "kube-system",
-            values = {
-                "image": { "pullPolicy": "IfNotPresent" },
-                "operator": { "replicas": 1 },  # No HA, this is a demo cluster
-
-                # Avoid `kube-proxy`, let Cilium sling packets around
-                #  requires `kubeProxyMode: "none"` in `kind-config.yaml`
-                "kubeProxyReplacement": True,
-                "k8sServiceHost": host,
-                "k8sServicePort": port,
-                # Optionally enable the Hubble observability tool
-                "hubble": {
-                    "relay": { "enabled": True },
-                    "ui": { "enabled": True },
-                } if "hubble" in features else {},
-            },
+        chart = "./cilium-1.17.2.tgz",  # FIXME pulumi somehow gets something else from the repo?
+        repository_opts = k8s.helm.v3.RepositoryOptsArgs(
+            repo = "https://helm.cilium.io/",
         ),
+        version = "1.17.2",  # TODO: autoupdate?
+        namespace = "kube-system",
+        # TODO signature verification?
+        values = {
+            "image": { "pullPolicy": "IfNotPresent" },
+            "operator": { "replicas": 1 },  # No HA, this is a demo cluster
+
+            # Avoid `kube-proxy`, let Cilium sling packets around
+            #  requires `kubeProxyMode: "none"` in `kind-config.yaml`
+            "kubeProxyReplacement": True,
+            "k8sServiceHost": host,
+            "k8sServicePort": port,
+
+            # Optionally enable the Hubble observability tool
+            "hubble": {
+                "relay": { "enabled": True },
+                "ui": { "enabled": True },
+            } if "hubble" in features else {},
+        },
     )
 
-    return cilium
+    return chart
