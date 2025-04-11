e729df99dcfc66f14330e8ac5ea1de25ea080788
nicoo <nicoo@mur.at>
2025-04-11 13:10:48 +0000

    minify example for discussion

--- __main__.py
+++ __main__.py
@@ -9,7 +9,7 @@ import cilium
 cfg = pulumi.Config()
 
 cilium_chart = cilium.deploy(cfg, features = {
-    'hubble', 'l7'
+    'l7'
 })
 
 pulumi.export(
@@ -23,42 +23,4 @@ pulumi.export(
         for [p, svc] in resources
         if p
     ]))
-)
-
-app_name = "nginx"
-app_labels = { "app": app_name }
-
-# Deploy Nginx
-deployment = Deployment(
-    app_name,
-    opts = pulumi.ResourceOptions(
-        depends_on = [ cilium_chart ],  # Necessary to ensure the pod is managed by cilium
-    ),
-    spec = {
-        "selector": { "match_labels": app_labels },
-        "replicas": 1,
-        "template": {
-            "metadata": { "labels": app_labels },
-            "spec": { "containers": [{ "name": app_name, "image": "nginx" }] }
-        },
-    },
-)
-
-# Allocate an IP to the Deployment.
-frontend = Service(
-    app_name,
-    metadata = {
-        "labels": deployment.spec["template"]["metadata"]["labels"],
-    },
-    spec = {
-        "type": "LoadBalancer",
-        "ports": [{ "port": 80, "target_port": 80, "protocol": "TCP" }],
-        "selector": app_labels,
-    }
-)
-
-ingress = frontend.status.load_balancer.apply(lambda v: v["ingress"][0] if "ingress" in v else "output<string>")
-pulumi.export(
-    "ip",
-    ingress.apply(lambda v: v["ip"] if v and "ip" in v else (v["hostname"] if v and "hostname" in v else "output<string>"))
 )
