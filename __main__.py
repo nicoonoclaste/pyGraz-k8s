6a3f23625f5a617b6638f1f73fb10c87ab46abf6
nicoo <nicoo@mur.at>
2025-04-19 14:48:49 +0000

    Add example app

--- __main__.py
+++ __main__.py
@@ -16,3 +16,15 @@ cilium_chart = cilium.deploy(cfg, features = {
 # Setup Nginx Gateway Fabric, as the Gateway API implementation
 #  see https://gateway-api.sigs.k8s.io/
 gateway = gateway.deploy(depends_on = [ cilium_chart ])
+
+# Demo application
+# taken from https://docs.nginx.com/nginx-gateway-fabric/get-started/
+demo_app = k8s.yaml.v2.ConfigFile(
+    "demo-app",
+    file = "./demo_app.yaml",
+)
+demo_routes = k8s.yaml.v2.ConfigFile(
+    "demo-routes",
+    file = "./demo_routes.yaml",
+    opts = pulumi.ResourceOptions(depends_on = [ gateway["crd"] ]),
+)
